ae8035ece92e9a78209e416c7398ab15b491d2e3
DiegoLozaRomero <20233tn010@utez.edu.mx>
2025-12-05 12:20:56 -0600

    Refactor Vite config to use defineConfig and plugins
    Updated Vite configuration to include React and ignore CSS errors.

--- Client-react/vite.config.js
+++ Client-react/vite.config.js
@@ -1,6 +1,12 @@
-export default {
-  build: {
-    minify: false
-  }
-}
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import Ignore from 'vite-plugin-ignore';
 
+export default defineConfig({
+  plugins: [
+    react(),
+    Ignore({
+      patterns: ['css'] // Ignora errores de CSS
+    })
+  ]
+});
