125aa2976d032c456c737a49cfa2e5576683b4e9
DiegoLozaRomero <20233tn010@utez.edu.mx>
2025-12-05 12:36:42 -0600

    Update vite.config.js

--- Client-react/vite.config.js
+++ Client-react/vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-// import Ignore from 'vite-plugin-ignore'; // ❌ eliminar o comentar
 
 export default defineConfig({
-  plugins: [
-    react(),
-    // Ignore({ patterns: ['css'] }) // ❌ eliminar o comentar
-  ]
+  plugins: [react()],
+  build: {
+    minify: false, // desactiva el minificador por ahora
+  },
 });
